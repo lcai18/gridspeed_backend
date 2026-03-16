1dd8fcff2f180430402e33c55b90ec62516e573e
lcai18 <lcai18@jh.edu>
2026-03-15 21:33:51 -0700

    fix: cors for new local ip

--- app.py
+++ app.py
@@ -15,6 +15,7 @@ app.add_middleware(
         "http://localhost:5174",
         "http://127.0.0.1:5174",
         "http://localhost:3000",
+        "http://127.0.0.1:5173",
     ],
     allow_credentials=True,
     allow_methods=["*"],
